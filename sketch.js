f5bee8ccca05cef2384dcae9561f531773bfcf6c
Pratik-coder11 <115369911+Pratik-coder11@users.noreply.github.com>
2022-10-10 23:32:07 +0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,39 +1,47 @@
-var box;
-
 function setup() {
-  createCanvas(400,400);
-  box = createSprite(200,200,30,30);
-
-}
-
-function draw() 
-{
-   background(30);
-
-  if (keyIsDown(RIGHT_ARROW)) 
-  {
-    box.position.x = box.position.x +5;
+    createCanvas(400,400);
+    background(51);
+    box = createSprite(200,200,30,30);
+  
   }
-
-    if (keyIsDown(LEFT_ARROW)) 
+  
+  function draw() 
   {
-    box.position.x= box.position.x -5;
+  
+    // write the code to change background color 
+    // to red when RIGHT_ARROW is pressed
     
-  }
- 
-    if (keyIsDown(UP_ARROW)) 
-  {
-    box.position.y = box.position.y - 5;
+    
+  
+    if (keyIsDown(LEFT_ARROW)) 
+    {
+      background("blue");
+      
+    }
    
+      if (keyIsDown(UP_ARROW)) 
+    {
+      background("yellow");
+     
+    }
+  
+    if (keyIsDown(DOWN_ARROW)) 
+    {
+      background("green");
+    }
+  
+    if (keyIsDown(RIGHT_ARROW)) 
+    {
+      background("purple");
+      
+    }
+  
+  
+  
+    
+    drawSprites();
   }
-
-  if (keyIsDown(DOWN_ARROW)) 
-  {
-    box.position.y = box.position.y + 5;
-  }
-
-  drawSprites();
-}
+  
 
 
 
